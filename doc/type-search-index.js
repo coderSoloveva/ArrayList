4bd08a86f3b37d1a1465584b4cda3f72ec7299cb
coderSoloveva <dorineas@student.21-school.ru>
2022-06-16 15:39:03 +0300

    add javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"MyArrayList"},{"p":"<Unnamed>","l":"MyList"},{"p":"<Unnamed>","l":"QuickSortComparable"},{"p":"<Unnamed>","l":"QuickSortComparator"}]
